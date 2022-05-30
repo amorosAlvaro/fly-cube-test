98ea0b37431900311b62fdb59fe2eda641deae3b
alvaro <amoros@bitsinmotion.de>
2022-05-30 17:05:16 +0200

    Add: getRandomUsers.test

---
+++ src/services/getRandomUsers.test.ts
@@ -0,0 +1,54 @@
+import getRandomUsers from './getRandomUsers';
+
+describe('when a random user is requested', () => {
+    it('if response is OK a randomUser object is returned', async () => {
+        // @ts-ignore
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+                ok: true,
+                json: () =>
+                    Promise.resolve({
+                        nat: 'US',
+                        picture: {
+                            large: 'large',
+                            medium: 'medium',
+                            thumbnail: 'thum',
+                        },
+                        registered: {
+                            age: 5,
+                            date: 'date',
+                        },
+                    }),
+            })
+        );
+
+        const randomUser = await getRandomUsers('testName');
+
+        expect(randomUser).toEqual({
+            nat: 'US',
+            picture: {
+                large: 'large',
+                medium: 'medium',
+                thumbnail: 'thum',
+            },
+            registered: {
+                age: 5,
+                date: 'date',
+            },
+            userName: 'testName',
+        });
+    });
+    it('if response is !ok, null and error message is returned', async () => {
+        // @ts-ignore
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+                ok: false,
+                status: 404,
+                statusText: 'error',
+            })
+        );
+        const randomUser = await getRandomUsers('testName');
+
+        expect(randomUser).toBeNull();
+    });
+});
